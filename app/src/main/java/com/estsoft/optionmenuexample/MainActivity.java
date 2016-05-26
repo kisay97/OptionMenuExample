85b4ef25e71b035ae81441e02d2afdb24edcd172
최규광 <kisay97@naver.com>
2016-05-26 09:27:01 +0900

    20160526-0926 : 코드로 메뉴 만들기

--- app/src/main/java/com/estsoft/optionmenuexample/MainActivity.java
+++ app/src/main/java/com/estsoft/optionmenuexample/MainActivity.java
@@ -12,4 +12,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE, 0, 0, "Menu1");
+        menu.add(Menu.NONE, 1, 1, "Menu2");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case 0:
+                Toast.makeText(this, "Menu1 selected", Toast.LENGTH_SHORT).show();
+                break;
+            case 1:
+                Toast.makeText(this, "Menu2 selected", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
